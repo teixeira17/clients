51e105f75adbeb516c9e711c535691584b44ce18
Lucas Teixeira <lucas.lemos97@live.com>
2021-08-03 17:09:44 -0300

    Seeding created

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 2, '37286897004', 2500.0, 'José da Silva');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 1, '70647543044', 2050.0, 'Marcelino de Souza');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 3, '82606976029', 3400.0, 'Rafael Enes');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 2, '01638122091', 1900.0, 'Emanoel Smarce');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 4, '98544704077', 9500.0, 'Fernando Cares');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 1, '77177311009', 2050.0, 'Rubens Barros');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 1, '31053644043', 1680.0, 'Gabriel Fuentes');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 2, '23135768090', 2450.0, 'Lucas Teixeira');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 3, '30605304084', 2800.0, 'Willian Diniz');
+insert into tb_client (birth_date, children, cpf, income, name) values (NOW(), 2, '79280384040', 3100.0, 'Thiago Franceschini');
