579e72a60144a5f62e91bbbbe8d60c18bf58b8c0
Lucas Teixeira <lucas.lemos97@live.com>
2021-08-04 14:09:27 -0300

    Insert created

--- backend/src/main/java/com/teixeira/clients/service/ClientService.java
+++ backend/src/main/java/com/teixeira/clients/service/ClientService.java
@@ -32,4 +32,25 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtpToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	
+	
+	
+
+	private void copyDtpToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		
+	}
+
 }
